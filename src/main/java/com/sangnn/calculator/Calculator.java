120bc4a92fd8299e3ca29cc506f460e5b848ead7
sang.n <stackops@snn-buil.novalocal>
2022-09-18 12:05:14 +0700

    Add sum logic, controller and unit test

---
+++ src/main/java/com/sangnn/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.sangnn.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class Calculator {
+
+     public int sum(int a, int b) {
+
+          return a + b;
+
+     }
+
+}
